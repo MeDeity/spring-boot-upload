f02d8380e9f1a48cb5799b6233b699201a15f031
MeDeity <langrenbule@gmail.com>
2018-11-05 23:37:27 +0800

    Initial commit

---
+++ src/main/java/com/example/upload/SpringBootUploadApplication.java
@@ -0,0 +1,12 @@
+package com.example.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUploadApplication.class, args);
+    }
+}
